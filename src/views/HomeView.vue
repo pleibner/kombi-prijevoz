d1f69e651b98234f3ad4b6070a223c3f65b58a4b
Petra Leibner <petra.domitrovic@gmail.com>
2025-10-11 18:27:33 +0200

    Delete Vue demo files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// Kombi prijevoz home page
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <div class="container">
+      <h1>Hello, world!</h1>
+      <p>Welcome to the Kombi Prijevoz home page.</p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.home {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+
+h1 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+  color: #333;
+}
+
+p {
+  font-size: 1.2rem;
+  color: #666;
+}
+</style>
